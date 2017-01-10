d4bff69ef7cb79d22ccd9093b44c40a3338df39f
cuberolopez96 <cuberolopez96@gmail.com>
2017-01-10 13:13:00 +0100

    init

---
+++ Modules/Entity/Videojuegos.php
@@ -0,0 +1,51 @@
+<?php 
+/**
+* 
+*/
+class Videojuegos 
+{
+	private $nombre;
+	private $fechasalida;
+	private $empresa;
+	private $descripcion;
+	private $plataforma;
+	private $pdo;
+	public function setNombre($nombre){
+		$this->nombre = $nombre;
+		return $this;
+	}
+	public function setFechaSalida($fechaSalida){
+		$this->fechasalida=$fechaSalida;
+		return $this;
+	}
+	public function setEmpresa($empresa){
+		$this->empresa=$empresa;
+		return $this;
+	}
+	public function setDescripcion($descripcion){
+		$this->descripcion=$descripcion;
+		return $this;
+	}
+	public function setPlataforma($plataforma){
+		$this->plataformas=$plataforma;
+		return $this;
+	}
+	public function save(){
+		$nombre = $this->nombre;
+		$plataforma = $this->plataforma;
+		$fechasalida = $this->fechasalida;
+		$descripcion = $this->descripcion;
+		$empresa =$this->empresa;
+
+		$this->pdo->exec("INSERT INTO videojuegos(nombre,fechasalida,descripcion,empresa,plataforma) VALUES('$nombre','$fechasalida','$descripcion','$empresa','$plataforma')");
+	}
+
+
+
+	function __construct($nombre,$fechasalida,$empresa,$descripcion,$plataforma)
+	{
+		$this->pdo=ConnectDB::getInstance(); 
+		$this->setNombre($nombre)->setPlataforma($plataforma)->setFechaSalida($fechasalida)->setEmpresa($empresa)->setDescripcion($descripcion)->save();
+	}
+}
+ ?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>
+	Wellcome
+	</title>
+	<link rel="stylesheet" href="Resources/css/init.css">
+</head>
+<body>
+	<?php require_once "Modules/Route.php" ?>
+</body>
+</html>
